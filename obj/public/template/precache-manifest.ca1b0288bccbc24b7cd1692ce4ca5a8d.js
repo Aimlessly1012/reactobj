8389666ab201b10801c94fcc48b18c78850b5c2d
Aimlessly1012 <444769150@qq.com>
2019-07-22 09:46:10 +0800

    first commit

---
+++ obj/public/template/precache-manifest.ca1b0288bccbc24b7cd1692ce4ca5a8d.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1be0ab3d721cf549f5508932a3b14263",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a934cccf19b2a20dccfc",
+    "url": "/static/css/main.57b89663.chunk.css"
+  },
+  {
+    "revision": "64869164c31ff1a90c38",
+    "url": "/static/js/2.bc35bbe6.chunk.js"
+  },
+  {
+    "revision": "a934cccf19b2a20dccfc",
+    "url": "/static/js/main.e890c8b7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2ddab40488b2f923b2c5795d9d1ed62d",
+    "url": "/static/media/Ct.2ddab404.jpg"
+  },
+  {
+    "revision": "d4cac4d6066e5ce67b16de927c693c7e",
+    "url": "/static/media/Hb.d4cac4d6.jpg"
+  },
+  {
+    "revision": "0233c55b2ed2155e77b220c1bd5f4f00",
+    "url": "/static/media/Jd.0233c55b.jpg"
+  },
+  {
+    "revision": "b4bd7c19283292417fc6cacf5e1505ea",
+    "url": "/static/media/Yj.b4bd7c19.jpg"
+  },
+  {
+    "revision": "93e98d93abe30ffb877e48a7c6629a5a",
+    "url": "/static/media/about11Street.93e98d93.jpg"
+  },
+  {
+    "revision": "0a0fd62fdabbc11ce38bdcc8a0ad2d35",
+    "url": "/static/media/banner1.0a0fd62f.jpg"
+  },
+  {
+    "revision": "c3932ec283064c06d6dd178bc510168d",
+    "url": "/static/media/banner2.c3932ec2.jpg"
+  },
+  {
+    "revision": "02c25b94bc7480bf6385fdbc5452acf3",
+    "url": "/static/media/banner3.02c25b94.jpg"
+  },
+  {
+    "revision": "c723bf89730bcbe220c304315bd564aa",
+    "url": "/static/media/banner4.c723bf89.jpg"
+  },
+  {
+    "revision": "4d7e102fa623af6369bfe5ecfa69c644",
+    "url": "/static/media/banner5.4d7e102f.jpg"
+  },
+  {
+    "revision": "5985660efa8f8ca9c0105077a29b4b47",
+    "url": "/static/media/fdj.5985660e.png"
+  },
+  {
+    "revision": "cd4cb89a16df19c249d4bb265bcfa03a",
+    "url": "/static/media/gg.cd4cb89a.png"
+  },
+  {
+    "revision": "f08246b13b57febe38c1eaa24bff993c",
+    "url": "/static/media/gjfs.f08246b1.png"
+  },
+  {
+    "revision": "f29fd540710419e115493c5696b8880b",
+    "url": "/static/media/icon.f29fd540.png"
+  },
+  {
+    "revision": "9e2b751514a98ba09d2679713c9fd8eb",
+    "url": "/static/media/logo.9e2b7515.png"
+  },
+  {
+    "revision": "3ab572b2b9e21bebfcff874762bb583e",
+    "url": "/static/media/logo2.3ab572b2.png"
+  },
+  {
+    "revision": "41d18c482319badeaed0cbcc99ec251e",
+    "url": "/static/media/pay.41d18c48.jpg"
+  }
+]);
